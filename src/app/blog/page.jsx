44d218ada9e22f2f7d03ad1a826c9d249ef4e13f
Naoto Kubota <kubogen3@gmail.com>
2024-06-18 13:34:41 +0900

    no-cors

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -7,6 +7,7 @@ import Image from 'next/image';
 async function getData() {
   const res = await fetch('https://my-iig9rk7mu-bokuhakubos-projects.vercel.app/api/posts', {
     cache: 'no-store',
+    mode: 'no-cors',
   });
   if (!res.ok) {
     throw new Error('Failed to fetch data');
