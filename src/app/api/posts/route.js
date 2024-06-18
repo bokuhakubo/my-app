611e80b2c2fa2f9d22357b8f2cc78ccf43a6349f
Naoto Kubota <kubogen3@gmail.com>
2024-06-18 14:04:30 +0900

    modified cors

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -8,8 +8,33 @@ export const GET = async (request) => {
 
         const posts = await Post.find();
 
-        return new NextResponse(JSON.stringify(posts), { status: 200 });
+        const headers = {
+            "Access-Control-Allow-Origin": "*", // すべてのオリジンからアクセスを許可
+            "Access-Control-Allow-Methods": "GET, OPTIONS", // 許可するHTTPメソッド
+            "Access-Control-Allow-Headers": "Content-Type", // 許可するヘッダー
+            "Access-Control-Allow-Credentials": "true", // 認証情報（クッキーなど）の送信を許可
+        };
+
+        return new NextResponse(JSON.stringify(posts), {
+            headers,
+            status: 200,
+        });
     } catch (err) {
         return new NextResponse('Database Error', { status: 500 });
     }
-}
+};
+
+// プリフライトリクエスト（OPTIONS）に対応
+export const OPTIONS = async () => {
+    const headers = {
+        "Access-Control-Allow-Origin": "*", // すべてのオリジンからアクセスを許可
+        "Access-Control-Allow-Methods": "GET, OPTIONS", // 許可するHTTPメソッド
+        "Access-Control-Allow-Headers": "Content-Type", // 許可するヘッダー
+        "Access-Control-Allow-Credentials": "true", // 認証情報（クッキーなど）の送信を許可
+    };
+
+    return new NextResponse(null, {
+        headers,
+        status: 200,
+    });
+};
