0b650c2bbf3fbd1e7ba3204c09672b2f52f3ced6
Naoto Kubota <kubogen3@gmail.com>
2024-06-18 13:46:23 +0900

    aa

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = {
-    async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'https://my-iig9rk7mu-bokuhakubos-projects.vercel.app/api/:path*', // 実際のAPIエンドポイントに置き換えてください
-        },
-      ];
-    },
-  };
-
 export default nextConfig;
